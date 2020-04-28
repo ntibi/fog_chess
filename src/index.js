78540bd21f229bb34e25131466ad7c0d30ddbd37
ntibi <nicolastibi@yahoo.fr>
2020-04-28 03:15:11 +0200

    unselectable

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from "./components/App"
 import "core-js"
 import "regenerator-runtime"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './utils.css';
 
 ReactDOM.render(<App />, document.getElementById("app"))
