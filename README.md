7f56850218bbed593912b6bdfbcb2737a1490087
ntibi <nicolastibi@yahoo.fr>
2020-09-19 17:10:03 +0200

    handle right click to cancel dragging but continue hinting

--- README.md
+++ README.md
@@ -6,9 +6,6 @@
 - [ ] use zobrist hashing for transposition
 
 
-- [ ] when you drop a piece to put it back, the hints should still render
-
-
 - [ ] benchmark perf
 
 
@@ -24,13 +21,7 @@
 - [ ] allow drawing/marking on board
 
 
-- [ ] the dragging doesn't stop on right mouse down but on right mouse up
-
-
 - [ ] measure the real tile size (might be 1px off)
 
 
 - [ ] the pieces seem ~1px off from the tile center
-
-
-- [ ] right clic drag stop does not clear the hints
--- src/components/Piece.js
+++ src/components/Piece.js
@@ -14,11 +14,23 @@ export default class Piece extends Component {
         this.state = {
             hint: false,
             disable_drag: false,
+            pos: null,
         }
         this.drop = this.drop.bind(this)
         this.hint = this.hint.bind(this)
         this.stop_hint = this.stop_hint.bind(this)
         this.mouse_down = this.mouse_down.bind(this)
+        this.onDrag = this.onDrag.bind(this)
+    }
+
+    onDrag(mousemove) {
+        const { x, y } = mousemove
+        this.setState({
+            pos: {
+                x,
+                y,
+            }
+        })
     }
 
     getMouse(event) {
@@ -40,14 +52,17 @@ export default class Piece extends Component {
         }
     }
 
-    drop() {
+    drop(event) {
+        this.setState({
+            pos: null,
+        })
+        if (!this.props.can_move)
+            return
+        this.stop_hint()
         if (this.state.disable_drag)
             return this.setState({
                 disable_drag: false,
             })
-        if (!this.props.can_move)
-            return
-        this.stop_hint()
         if (!this.props.turn)
             return
         const mouse = this.getMouse(event);
@@ -56,8 +71,9 @@ export default class Piece extends Component {
             y: clamp(parseInt(mouse.y / this.props.tileSize), 0, 7),
         }
 
-        if (this.props.moves.find(move => same_coords(move, destination)))
+        if (this.props.moves.find(move => same_coords(move, destination))) {
             this.props.move(this.props.coords, destination)
+        }
     }
 
     hint() {
@@ -82,13 +98,14 @@ export default class Piece extends Component {
 
     mouse_down(e) {
         switch (e.button) {
-            case 0:
+            case 0: // left click
                 if (this.props.ally) {
                     e.stopPropagation()
-                    this.props.select(this.props.coords)
+                    if (!this.props.selected)
+                        this.props.select(this.props.coords)
                 }
                 break;
-            case 2:
+            case 2: // right click
                 this.stop_dragging()
                 break;
         }
@@ -112,29 +129,28 @@ export default class Piece extends Component {
                     key={newId("hint")}
                 />)
 
+        const position = this.state.pos && !this.state.disable_drag ? this.state.pos : { x: coords.x * tileSize, y: coords.y * tileSize, }
         return (
             <>
-            <Draggable
-                position={{
-                    x: coords.x * tileSize,
-                    y: coords.y * tileSize,
-                }}
-                bounds={{ left: -tileSize / 2, top: -tileSize / 2, right: tileSize * 7.5, bottom: tileSize * 7.5 }}
-                onStop={this.drop}
-                onStart={this.hint}
-                defaultClassNameDragging="dragged"
-                allowAnyClick={false}
-                onMouseDown={this.mouse_down}
-            >
-                <img
-                    className="piece"
-                    style={style}
-                    draggable={false}
-                    src={pieces[this.props.color][this.props.type]}
-                    alt={`${this.props.type}${this.props.color}`}
-                />
-            </Draggable>
-            {hints}
+                <Draggable
+                    position={position}
+                    bounds={{ left: -tileSize / 2, top: -tileSize / 2, right: tileSize * 7.5, bottom: tileSize * 7.5 }}
+                    onDrag={this.onDrag}
+                    onStop={this.drop}
+                    onStart={this.hint}
+                    defaultClassNameDragging="dragged"
+                    allowAnyClick={false}
+                    onMouseDown={this.mouse_down}
+                >
+                    <img
+                        className="piece"
+                        style={style}
+                        draggable={false}
+                        src={pieces[this.props.color][this.props.type]}
+                        alt={`${this.props.type}${this.props.color}`}
+                    />
+                </Draggable>
+                {hints}
             </>
         )
     }
