05485d627520076071b53f111e264896cc8b6872
ntibi <nicolastibi@yahoo.fr>
2020-09-21 00:48:45 +0200

    source

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import FPS from "react-fps-stats"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import "./App.css"
 
 export default class App extends Component {
     constructor(props) {
@@ -12,14 +13,19 @@ export default class App extends Component {
 
     render() {
         return (
-            <Container>
-                <Jumbotron>
-                    <FPS />
-                    <Board
-                        controls={"w"}
-                    />
-                </Jumbotron>
-            </Container>
+            <div>
+                <Container>
+                    <Jumbotron>
+                        <FPS />
+                        <Board
+                            controls={"w"}
+                        />
+                    </Jumbotron>
+                </Container>
+                <div className="source">
+                    <a href="https://github.com/ntibi/fog_chess">source</a>
+                </div>
+            </div>
         )
     }
 }
