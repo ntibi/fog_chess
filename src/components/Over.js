c706cdc97f827759d7f2d6c66f3d17a831f82baa
ntibi <nicolastibi@yahoo.fr>
2020-10-03 14:06:49 +0200

    game over

--- src/components/Over.js
+++ src/components/Over.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import "./Over.css"
 
-export default class Over extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
-
-    render() {
-        return (
-            <div
-                className="over"
-            >
-                <div>
-                    <p>you {this.props.won ? "won" : "lost"}</p>
-                    <button onClick={this.props.restart}>restart</button>
-                </div>
+export default function Over(props) {
+    return (
+        <div
+            className="over"
+        >
+            <div>
+                <p>you {props.won ? "won" : "lost"}</p>
+                <button onClick={props.restart}>restart</button>
             </div>
-        )
-    }
+        </div>
+    )
 }
