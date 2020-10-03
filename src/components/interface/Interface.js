8e863e17eb77af65f448579be3e334557e77f7f1
ntibi <nicolastibi@yahoo.fr>
2020-10-03 14:07:03 +0200

    wording

--- src/components/interface/Interface.js
+++ src/components/interface/Interface.js
@@ -5,7 +5,7 @@ export default function Interface(props) {
     return (
         <div className="interface">
             <div className="group">
-                <p>computer level {props.level.current}</p>
+                <p>computer depth {props.level.current}</p>
                 <input type="range" min={String(props.level.min)} max={String(props.level.max)} value={String(props.level.current)} className="slider" onChange={(e) => {props.set_level(Number(e.target.value))}} />
                 <div className={`spinner ${props.thinking && "spinning"}`}></div>
             </div>
