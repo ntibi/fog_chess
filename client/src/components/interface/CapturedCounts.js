dbc98c9c5193270a828be6496d35957e5e4f2224
ntibi <nicolastibi@yahoo.fr>
2020-11-23 22:21:52 +0100

    optimized CapturedCounts.js

--- client/src/components/interface/CapturedCounts.js
+++ client/src/components/interface/CapturedCounts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import setup from "../../game/setup";
 import { colors, score, color_name, other_color } from "../../game/rules";
 import "./CapturedCounts.css";
@@ -6,29 +6,35 @@ import CapturedPieces from "./CapturedPieces";
 
 export default function CapturedCounts({ pieces, controls }) {
 
-  const players = {};
-  for (let color of colors) {
-    const default_pieces = setup.filter(piece => color === piece.color && piece.type !== "k");
-    const default_pieces_counts = {};
-    default_pieces.forEach(({ type }) => default_pieces_counts[type] = (default_pieces_counts[type] || 0) + 1);
-    const alive_pieces = pieces.filter(piece => color === piece.color && piece.type !== "k");
-    const alive_pieces_counts = {};
-    alive_pieces.forEach(({ type }) => alive_pieces_counts[type] = (alive_pieces_counts[type] || 0) + 1);
-    const value = Object.keys(alive_pieces_counts).reduce((acc, type) => acc + (alive_pieces_counts[type] || 0) * score[type], 0);
-    players[color] = {
-      default_pieces_counts,
-      alive_pieces_counts,
-      value,
-    };
-  }
+  const players = useMemo(() => {
+    const players = {};
 
-  for (let color of colors) {
-    const other = other_color(color);
-    const eaten_pieces_counts = {};
-    Object.keys(players[other].default_pieces_counts).forEach(key =>
-      eaten_pieces_counts[key] = players[other].default_pieces_counts[key] - (players[other].alive_pieces_counts[key] || 0));
-    players[color].eaten_pieces_counts = eaten_pieces_counts;
-  }
+    for (let color of colors) {
+      const default_pieces = setup.filter(piece => color === piece.color && piece.type !== "k");
+      const default_pieces_counts = {};
+      default_pieces.forEach(({ type }) => default_pieces_counts[type] = (default_pieces_counts[type] || 0) + 1);
+      const alive_pieces = pieces.filter(piece => color === piece.color && piece.type !== "k");
+      const alive_pieces_counts = {};
+      alive_pieces.forEach(({ type }) => alive_pieces_counts[type] = (alive_pieces_counts[type] || 0) + 1);
+      const value = Object.keys(alive_pieces_counts).reduce((acc, type) => acc + (alive_pieces_counts[type] || 0) * score[type], 0);
+      players[color] = {
+        default_pieces_counts,
+        alive_pieces_counts,
+        value,
+      };
+    }
+
+    for (let color of colors) {
+      const other = other_color(color);
+      const eaten_pieces_counts = {};
+      Object.keys(players[other].default_pieces_counts).forEach(key =>
+        eaten_pieces_counts[key] = players[other].default_pieces_counts[key] - (players[other].alive_pieces_counts[key] || 0));
+      players[color].eaten_pieces_counts = eaten_pieces_counts;
+    }
+
+    return players;
+  }, [ pieces, controls ]);
+
 
   return (
     <div>
